7dcc72cb40f933f665ca087ed22817e7f45c1f99
v074 <buncherd01@gmail.com>
2021-02-26 10:44:49 -0600

    Reinitialization step 1: adding index, style, and scripts files.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function openAbout(){
+    document.getElementById("about").style.width = "80%";
+    document.getElementById("about").style.borderWidth = "5px";
+}
+
+function closeAbout(){
+    document.getElementById("about").style.width = "0";
+    document.getElementById("about").style.borderWidth = "0px";
+}
+
+function openTech(){
+    document.getElementById("technologies").style.width = "80%";
+    document.getElementById("technologies").style.borderWidth = "5px";
+}
+
+function closeTech(){
+    document.getElementById("technologies").style.width = "0";
+    document.getElementById("technologies").style.borderWidth = "0px";
+}
+
+function openProjects(){
+    document.getElementById("projects").style.width = "80%";
+    document.getElementById("projects").style.borderWidth = "5px";
+}
+
+function closeProjects(){
+    document.getElementById("projects").style.width = "0";
+    document.getElementById("projects").style.borderWidth = "0px";
+}
+
+// As contact form handling via PHP is not possible on Github, these are being commented out and left in for legacy reference.
+// function openContact(){
+//     document.getElementById("contact").style.width = "80%";
+//     document.getElementById("contact").style.borderWidth = "5px";
+// }
+
+// function closeContact(){
+//     document.getElementById("contact").style.width = "0";
+//     document.getElementById("contact").style.borderWidth = "0px";
+// }
