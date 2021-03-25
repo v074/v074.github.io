b31245cc5d62b7898a3e85847d4615e393f452f5
v074 <buncherd01@gmail.com>
2021-03-25 11:44:23 -0400

    Reverting javascript file to previous form: latest update had broken the javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,43 @@
 function openAbout(){
-    document.getElementById("about").classList.add(`opened`);
+    document.getElementById("about").style.width = "80%";
+    document.getElementById("about").style.borderWidth = "5px";
 }
 
 function closeAbout(){
-    document.getElementById("about").classList.remove(`opened`);
+    document.getElementById("about").style.width = "0";
+    document.getElementById("about").style.borderWidth = "0px";
 }
 
-function openTech(){    
-    document.getElementById("technologies").classList.add(`opened`);
+function openTech(){
+    document.getElementById("technologies").style.width = "80%";
+    document.getElementById("technologies").style.borderWidth = "5px";
 }
 
 function closeTech(){
-    document.getElementById("technologies").classList.add(`opened`);
+    document.getElementById("technologies").style.width = "0";
+    document.getElementById("technologies").style.borderWidth = "0px";
 }
 
 function openProjects(){
-    document.getElementById("projects").classList.add(`opened`);
+    document.getElementById("projects").style.width = "80%";
+    document.getElementById("projects").style.borderWidth = "5px";
 }
 
 function closeProjects(){
-    document.getElementById("projects").classList.remove(`opened`);
+    document.getElementById("projects").style.width = "0";
+    document.getElementById("projects").style.borderWidth = "0px";
 }
+
+// As contact form handling via PHP is not possible on Github, these are being commented out and left in for legacy reference.
+// function openContact(){
+//     document.getElementById("contact").style.width = "80%";
+//     document.getElementById("contact").style.borderWidth = "5px";
+// }
+
+// function closeContact(){
+//     document.getElementById("contact").style.width = "0";
+//     document.getElementById("contact").style.borderWidth = "0px";
+// }
 
 // As contact form handling via PHP is not possible on Github, these are being commented out and left in for legacy reference.
 // function openContact(){
